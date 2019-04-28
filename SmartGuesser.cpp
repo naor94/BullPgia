15f2b347bdea6153f4a523656decd80f6e342ce1
naor94 <44939883+naor94@users.noreply.github.com>
2019-04-28 14:54:33 +0300

    Add files via upload

---
+++ SmartGuesser.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "SmartGuesser.hpp"
+using namespace bullpgia;
+#include <string>
+
+string SmartGuesser::guess(){
+    return "gfgf";
+}
+
+void SmartGuesser::startNewGame(uint length) {
+}
+void SmartGuesser::learn(string reply){
+
+
+}
