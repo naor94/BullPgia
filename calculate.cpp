d8d526341051ccf558f306f8a5b62858c66d3e69
naor94 <44939883+naor94@users.noreply.github.com>
2019-05-01 10:43:07 -0400

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -6,29 +6,69 @@ using namespace std;
 
 
 
-   string bullpgia::calculateBullAndPgia( string x, string y){
+   string bullpgia::calculateBullAndPgia( string choose, string guess){
+        int len_c = choose.size();
+        int len_g=guess.size();
         int count_bull=0;
         int count_pgia=0;
         string s;
-        for(int i = 0; i < x.length(); i++)
+        int array_g [guess.size()];
+        int array_c [choose.size()];
+        int i = 0;
+        for(; i < len_c; i++)
         {
-            for(int j = 0; j< y.length(); j++)
-            {
-               if(i==j&&x.at(i)==y.at(j)){
-                   count_bull++;
-               }
-               else if(x.at(i)==y.at(j)){
-                   count_pgia++;
-               }
-            }
+          array_c[i]=(int)(choose.at(i)-48);
+        }
+         i=0;
+        for(; i < len_g; i++)
+        {
+          array_g[i]=(int)(guess.at(i)-48);
+        }
+         i=0;
+         int minlen=len_c<len_g?len_c:len_g;
+        for(; i < minlen; i++)
+         {
+             
+            if(array_c[i]==array_g[i]){
+               count_bull++;
+               array_c[i]=-1;
+               array_g[i]=-1;
+              
+
+           }
+          
+           
+
+           
             
         }
-       string bull=to_string(count_bull);
-       string pgia=to_string(count_pgia);
-       s=bull+","+pgia;
+      i=0;
+        for(; i < len_c; i++)
+        {
+           for(int j = 0; j < len_g; j++)
+           {
+               if(((array_c[i]!=-1) && (array_g[j]!=-1)) && (array_c[i]==array_g[j])){
+                   count_pgia++;
+                   array_c[i]=-1;
+                   array_g[j]!=-1;
+
+           }
+           
+        }
+        
+
+        }
+        
+        
+       string bull_1=to_string(count_bull);
+       string pgia_1=to_string(count_pgia);
+       s=bull_1+","+pgia_1;
         
         
     return s;
+
+
+    
 }
 
 
